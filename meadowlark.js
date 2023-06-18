6f55143b92190c6be9b3c7ab995d1fed3cbbf962
Duziem Ugoji <tickettoduzz@gmail.com>
2023-06-18 18:46:38 +0100

    refactored into handlers

--- meadowlark.js
+++ meadowlark.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
 
-const fortune = require('./lib/fortune');
-
+const handlers = require('./lib/handlers')
 
 const app = express()
 
@@ -11,24 +10,13 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (request, response) => response.render('home'))
+app.get('/', handlers.home)
 
-app.get('/about', (request, response) => {
-  response.render('about', { fortune: fortune.getFortune() })
-})
+app.get('/about', handlers.about)
 
-app.use((request, response) => {
-  // response.type('text/html')
-  // the view engine returns a content type of text/html and a status code of 200, by default
-  response.status(404)
-  response.render('404')
-})
+app.use(handlers.notFound)
 
-app.use((err, request, response, next) => {
-  console.error(err.message)
-  response.status(500)
-  response.render('500')
-})
+app.use(handlers.serverError)
 
 
 const PORT = process.env.PORT || 3000;
