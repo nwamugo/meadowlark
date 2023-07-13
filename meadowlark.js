7fe2b711691be63922ab8aafea83366383f42e89
Duziem Ugoji <tickettoduzz@gmail.com>
2023-07-13 17:25:55 +0100

    routing

--- meadowlark.js
+++ meadowlark.js
@@ -14,10 +14,22 @@ const flashMiddleware = require('./lib/middleware/flash')
 const cartValidation = require('./lib/middleware/cartValidation')
 
 const { credentials } = require('./config')
+const vhost = require('vhost')
 
 require('./db')
+const addRoutes = require('./routes')
+addRoutes(app)
 
 const app = express()
+// create "admin" subdomain... this should appear
+// before all your other routes
+const admin = express.Router()
+// express.Router() essentially creates a new instance of the Express
+// router. You can treat this instance just like your original instance (app).
+// You can add routes and middleware just as you would to app. However, it
+// won’t do anything until you add it to app. We add it through vhost, which
+// binds that router instance to that subdomain.
+app.use(vhost('admin.meadowlark.local', admin))
 
 const hbs = create({
   defaultLayout: 'main',
@@ -71,6 +83,10 @@ app.use(cartValidation.resetValidation)
 app.use(cartValidation.checkWaivers)
 app.use(cartValidation.checkGuestCounts)
 
+app.get('/page-with-specials', handlers.specials, handlers.specialsPage)
+
+app.get('/secret', handlers.authorize, () => res.render('secret'))
+
 app.get('/', handlers.home)
 
 app.get('/about', handlers.about)
@@ -82,7 +98,7 @@ app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
 app.post(
   '/newsletter-signup/thank-you',
   handlers.newsletterSignupThankYou
-)
+  )
 
 app.get('vacations', handlers.listVacations)
 app.get('/contest/vacation-photo', handlers.vacationPhotoContest)
@@ -93,10 +109,10 @@ app.get(
 app.get(
   '/contest/vacation-photo-thank-you',
   handlers.vacationPhotoContestProcessThankYou
-)
-app.post('/contest/vacation-photo/:year/:month', (req, res) => {
-  const form = new multiparty.Form()
-  form.parse(req, (err, fields, files) => {
+  )
+  app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+    const form = new multiparty.Form()
+    form.parse(req, (err, fields, files) => {
     if (err) return handlers.vacationPhotoContestProcessError(req, res)
     console.log('got fields: ', fields)
     console.log('and files: ', files)
@@ -112,13 +128,56 @@ app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
   const form = new multiparty.Form()
   form.parse(req, (err, fields, files) => {
     if (err) return handlers.api
-      .vacationPhotoContestError(req, res, err.message)
+    .vacationPhotoContestError(req, res, err.message)
     handlers.api.vacationPhotoContest(req, res, fields, files)
   })
 })
 
+const staff = {
+  mitch: { name: "Mitch",
+    bio: 'Mitch is the man to have at your back in a bar fight.' },
+  madeline: { name: "Madeline", bio: 'Madeline is our Oregon expert.' },
+  walt: { name: "Walt", bio: 'Walt is our Oregon Coast expert.' },
+}
+
+app.get('/staff/:name', (req, res, next) => {
+  const info = staff[req.params.name]
+  if(!info) return next()   // will eventually fall through to 404
+  res.render('05-staffer', info)
+})
+
+app.get('/staff', (req, res) => {
+  res.render('05-staff', { staffUrls: Object.keys(staff).map(key => '/staff/' + key) })
+})
+
+
+const autoViews = {}
+const { promisify } = require('util')
+const fileExists = promisify(fs.exists)
+
+app.use(async (req, res, next) => {
+  const path = req.path.toLowerCase()
+  try {
+    // check cache; if it's there, render the view
+    if (autoViews[path]) return res.render(autoViews[path])
+    // if its' not in the cache, see if there's
+    // a .handlebars file that matches
+    if(await fileExists(__dirname + '/views' + path + '.handlebars')) {
+      autoViews[path] = path.replace(/^\//, '')
+      return res.render(autoViews[path])
+    }
+    // no view found; pass on to 404 handler
+    next()
+  } catch (error) {
+    console.error(error)
+  }
+})
+
 app.use(handlers.notFound)
 app.use(handlers.serverError)
+
+//create admin routes; these can be defined anywhere
+admin.use('*', (req, res) => res.send('Welcome, Admin!'))
 
 
 
